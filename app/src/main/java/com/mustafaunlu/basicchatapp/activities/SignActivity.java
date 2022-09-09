9d48b6c9d500328dbd9103e702b7d95cb9cb0091
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-09 12:22:13 +0300

    First touch!

---
+++ app/src/main/java/com/mustafaunlu/basicchatapp/activities/SignActivity.java
@@ -0,0 +1,16 @@
+package com.mustafaunlu.basicchatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mustafaunlu.basicchatapp.R;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+    }
+}

86774894560503c84fbd1ba2886203dfd66b3f10
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-09 14:33:44 +0300

    Menu design and linking menu with ChatActivity

--- app/src/main/java/com/mustafaunlu/basicchatapp/activities/ChatActivity.java
+++ app/src/main/java/com/mustafaunlu/basicchatapp/activities/ChatActivity.java
@@ -1,9 +1,14 @@
 package com.mustafaunlu.basicchatapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mustafaunlu.basicchatapp.R;
 
 public class ChatActivity extends AppCompatActivity {
@@ -12,5 +17,28 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.toProfileActivity){
+            //Intent
+
+        }
+        else if(item.getItemId() == R.id.signOut){
+            //Sign out
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

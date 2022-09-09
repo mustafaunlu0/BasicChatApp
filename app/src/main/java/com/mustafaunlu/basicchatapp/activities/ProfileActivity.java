78071c012d413f85b10ef017e1e4d89de41869b4
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-09 17:45:33 +0300

    ProfileActivity completed.

--- app/src/main/java/com/mustafaunlu/basicchatapp/activities/ProfileActivity.java
+++ app/src/main/java/com/mustafaunlu/basicchatapp/activities/ProfileActivity.java
@@ -1,21 +1,207 @@
 package com.mustafaunlu.basicchatapp.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.mustafaunlu.basicchatapp.R;
+import com.mustafaunlu.basicchatapp.databinding.ActivityProfileBinding;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ActivityProfileBinding binding;
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+
+    Uri selectedUri;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding=ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth=FirebaseAuth.getInstance();
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+
+        storageReference=firebaseStorage.getReference();
+
+
+        getData();
+
+
     }
+
 
     public void upload(View view){
 
+        UUID uuid=UUID.randomUUID();
+        String imageName="images/"+uuid+".jpg";
+
+        StorageReference newReference=storageReference.child(imageName);
+
+        //Storage a kayıt
+        newReference.putFile(selectedUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                //FireStore a kayıt
+
+                String downloadURL=selectedUri.toString();
+                String userAge=binding.ageEditText.getText().toString();
+                FirebaseUser user=mAuth.getCurrentUser();
+
+                Map<String, Object> profileMap=new HashMap<>();
+                profileMap.put("ProfileURI",downloadURL);
+                profileMap.put("Age",userAge);
+                profileMap.put("Mail",user.getEmail());
+
+
+                firebaseFirestore.collection("Profiles").add(profileMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        Toast.makeText(ProfileActivity.this, "Datum recorded!", Toast.LENGTH_LONG).show();
+                        Intent intent=new Intent(ProfileActivity.this,ChatActivity.class);
+                        startActivity(intent);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ProfileActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProfileActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
+    }
+    private void getData() {
+
+        firebaseFirestore.collection("Profiles").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+
+                    for(QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+                        Map<String,Object> map=queryDocumentSnapshot.getData();
+
+                        String userMail= (String) map.get("Mail");
+
+                        if(userMail.matches(mAuth.getCurrentUser().getEmail())){
+                            String userAge= (String) map.get("Age");
+                            String userImage= (String) map.get("ProfileURI");
+
+                            if(userAge != null && userImage != null){
+                                Picasso.get().load(userImage).into(binding.imageView);
+                                binding.ageEditText.setText(userAge);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProfileActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    public void selectImage(View view) {
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            //permission needed
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
+        }else{
+            //permission granted
+            Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,2);
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,2);
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+
+            selectedUri=data.getData();
+
+            try{
+                Bitmap selectedBitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),selectedUri);
+                binding.imageView.setImageBitmap(selectedBitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }

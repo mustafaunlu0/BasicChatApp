461a36ef3b3a22f0f34668569cb87b8f092f2369
mustafaunlu0 <79142435+mustafaunlu0@users.noreply.github.com>
2022-09-09 14:56:21 +0300

    ProfileActivity design

---
+++ app/src/main/java/com/mustafaunlu/basicchatapp/activities/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.mustafaunlu.basicchatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import com.mustafaunlu.basicchatapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void upload(View view){
+
+    }
+}
